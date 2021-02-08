b3c48fa5b33127e59d354dd6c8198c8c7c784441
cfenc <cfenc@DESKTOP-QFJHHIJ>
2021-02-08 15:37:43 +0100

    Add project files.

---
+++ BeeldHerkenning Eindopdracht Wolf en Auto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeeldHerkenning_Eindopdracht_Wolf_en_Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
